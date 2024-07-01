57ccdb6633770be462b895480459561baad56b20
Mike Sperber <sperber@deinprogramm.de>
2024-07-01 15:40:15 +0200

    Update.

--- racket/table-format.rkt
+++ racket/table-format.rkt
@@ -1,1 +1,20 @@
 #lang racket
+; Ein Tabellenformat (hat einen Typ) ist ...
+; - Cell -ODER-
+; - Record -ODER-
+; - Sequence
+
+; Cell ist ...
+; - eine Int-Cell -ODER-
+; - eine Double-Cell -ODER-
+; - eine Datum-Cell -ODER-
+; - eine Aufzählungs-Cell -ODER- ...
+
+; Value: "Typ des Formats"
+
+; Funktion: String -> Value -ODER- error
+(struct cell
+  (validator-function))
+
+(struct error
+  (description))
