159c45f67db9b08918cca0e4b49d352a8d622977
Mike Sperber <sperber@deinprogramm.de>
2024-07-01 11:25:55 +0200

    Update.

--- racket/intro.rkt
+++ racket/intro.rkt
@@ -10,3 +10,8 @@
 (define circle1 (circle 50 "solid" "gold"))
 (define square1 (square 100 "solid" "blue"))
 (define star1 (star 50 "solid" "red"))
+
+(define overlay1 (overlay star1 circle1))
+(define image1 (beside overlay1 square1))
+
+; Zeilenkommentar
