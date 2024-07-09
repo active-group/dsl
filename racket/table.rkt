f87cde0c49a7a11a2240966f0442838bd159a1e6
Mike Sperber <sperber@deinprogramm.de>
2024-07-09 16:12:54 +0200

    Update.

--- racket/table.rkt
+++ racket/table.rkt
@@ -11,10 +11,40 @@
 
 ; fehlt: räumliche Anordnung
 
-; Eine Tabelle(nformat) ist eins der folgenden:
+; Eine Tabellenformat) ist eins der folgenden:
 ; - eine einzelne Zelle -ODER-
 ; - ein Datensatz bestehend aus mehreren Zellen -ODER-
 ; - Folge aus beliebig vielen Datensätzen
+
+; Ein Zellenformat ist eins der folgenden:
+(struct enum-cell
+  (row
+   column
+   values)) ; Liste möglicher konstante Werte
+
+(define segment-format
+  (enum-cell 0 0 '("Government" "Midmarket")))
+
+#;(define segment-header-format
+  (enum-cell 0 0 '("Segment")))
+
+(define (header-format header-text)
+  (enum-cell 0 0 (cons header-text '())))
+
+(define segment-header-format
+  (header-format "Segment"))
+
+(struct integer-cell
+  (row
+   column))
+
+(struct string-cell
+  (row
+   column))
+
+
+
+; Header-Zelle
 
 (struct profitability
   (segment
