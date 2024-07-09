76910bd82d98ccd9177f172410f005ae34deb1f1
Mike Sperber <sperber@deinprogramm.de>
2024-07-09 15:34:53 +0200

    Update.

--- racket/table.rkt
+++ racket/table.rkt
@@ -1,1 +1,20 @@
 #lang racket
+; Tabellenformat
+
+; Datentypen in den einzelnen Zellen
+
+; Datensatz pro Zeile / pro ??? gibt's auch in rechteckig
+
+; Header/Leerzeile (oder nicht)
+
+; Liste
+
+(struct profitability
+  (segment
+   country
+   units-sold
+   manuf-price
+   sale-price
+   sales
+   profit)
+  #:transparent)
