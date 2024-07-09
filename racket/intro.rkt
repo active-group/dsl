315868870875559719b895a3bcfab9755ca5833a
Mike Sperber <sperber@deinprogramm.de>
2024-07-09 10:29:08 +0200

    Update.

--- racket/intro.rkt
+++ racket/intro.rkt
@@ -5,3 +5,19 @@
   (+ 12
      (* 5
         4)))
+
+(define circle1 (circle 50 "solid" "yellow"))
+(define square1 (square 100 "outline" "green"))
+(define star1 (star 50 "solid" "red"))
+
+(define overlay1 (overlay star1 circle1))
+
+; overlay, beside, above: Kombinatoren
+
+(above
+ (beside circle1 star1)
+ (beside star1 circle1))
+
+(above
+ (beside square1 circle1)
+ (beside circle1 square1))
