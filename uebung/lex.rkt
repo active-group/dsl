a7d74b9133288bc99d04d9c5a288516b266f57a9
Mike Sperber <sperber@deinprogramm.de>
2024-07-11 12:49:50 +0200

    Lexer aus Übung

---
+++ uebung/lex.rkt
@@ -0,0 +1,58 @@
+#lang racket/base
+(require parser-tools/lex
+         (prefix-in : parser-tools/lex-sre))
+
+(define-tokens dtokens (KEY IDENTIFIER STRING))
+(define-empty-tokens dpunct (LPAREN COMMA RPAREN EOF))
+(define-lex-abbrev id-chars (char-complement (char-set "(,)=:.~?\"% \n")))
+(define-lex-abbrev identifier-re (:: id-chars (:* (:or upper-case id-chars))))
+(define-lex-abbrev string-chars (char-complement (char-set "\"")))
+(define-lex-abbrev string-re (:: #\" (:* string-chars) #\"))
+(define-lex-abbrev key-re (:: identifier-re #\:))
+
+
+(define dlexer
+  (lexer-src-pos
+   [whitespace
+    (return-without-pos (dlexer input-port))]
+   [identifier-re
+    (token-IDENTIFIER lexeme)]
+   [key-re
+    (token-KEY (substring lexeme 0 (- (string-length lexeme) 1)))]
+   [string-re
+    (token-STRING (substring lexeme 1 (- (string-length lexeme) 1)))]
+   [#\( (token-LPAREN)]
+   [#\, (token-COMMA)]
+   [#\) (token-RPAREN)]
+   [(eof) (token-EOF)]))
+
+(provide dtokens dpunct
+         id-chars string-re identifier-re
+         dlexer)
+
+(module+ test
+  (require rackunit)
+  (define (test-lexer str tok-name [tok-value str])
+    (define pv (dlexer (open-input-string str)))
+    (define v (position-token-token pv))
+    (test-equal? (format "lexer: ~a: <~a,~a>" str tok-name tok-value)
+                 (cons (token-name v) (token-value v))
+                 (cons tok-name tok-value)))
+  (define (get-tokens input-port)
+    (let ((with-position (lambda () (dlexer input-port))))
+      (let loop ((tokens '()))
+        (let ((token (position-token-token (with-position))))
+          (if (eq? (token-name token) 'EOF)
+              (reverse tokens)
+              (loop (cons token tokens)))))))
+
+  (define (get-tokens-from-file filename)
+    (call-with-input-file filename get-tokens))
+  
+  (test-lexer "(" 'LPAREN #false)
+  (test-lexer ")" 'RPAREN #false)
+  (test-lexer "," 'COMMA #false)
+  (test-lexer "-abc" 'IDENTIFIER "-abc")
+  (test-lexer "\"test\"" 'STRING "test")
+  (get-tokens-from-file "test.rkt")
+  )
---
+++ uebung/test.rkt
@@ -0,0 +1,11 @@
+
+default-data-set:
+  format: profitabilities-sheet
+  record: profitability
+column:(segment-format, segment, "Segment")
+column:(country-format, country, "Country")
+column:(integer, units-sold, "Units Sold")
+column:(integer, manufacturing-price, "Manuf. Price")
+column:(integer, sale-price, "Sale Price")
+column:(integer, sales, "Sales")
+column:(integer, profit, "Profit")
