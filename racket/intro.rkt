aa4af335c966488c9c82bf2a5532af2eacc21a28
Mike Sperber <sperber@deinprogramm.de>
2025-07-23 10:20:58 +0200

    Update.

--- racket/intro.rkt
+++ racket/intro.rkt
@@ -1,3 +1,18 @@
 #lang racket
+(require 2htdp/image)
+
 (define x
   (+ 12 (* 34 12)))
+
+(define circle1
+  (circle 50 "solid" "red"))
+(define square1
+  (square 100 "outline" "blue"))
+(define star1
+  (star 50 "solid" "gold"))
+
+; overlay: 2 Bilder rein, 1 Bild raus
+(define overlay1
+  (overlay star1 circle1))
+(define p1
+  (beside overlay1 square1))
