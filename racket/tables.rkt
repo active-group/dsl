7fc7e258ca98433a14644082efa84032b0b44b82
Mike Sperber <sperber@deinprogramm.de>
2025-07-23 15:31:16 +0200

    Update.

--- racket/tables.rkt
+++ racket/tables.rkt
@@ -69,4 +69,25 @@ Table:
 
 t
 
+; Tabelleninhalt: Funktion: x y -> Inhalt der Tabelle
+(define (table0 x y) ; Tablle, wo überall "Mike"
+  "Mike")
 
+(define (table1 x y)
+  (if (even? x)
+      "Mike"
+      "Tobias"))
+
+(define unit-list
+  '(("Segment" "Country" "Units" "Sold,Manuf." "Price" "Sale" "Price" "Sales" "Profit")
+    ("Government" "Canada" 1618 "$3,00" "$20,00" "$32.370,00" "$16.185,00")
+    ("Government" "Germany" 1321 "$3,00" "$20,00" "$26.420,00" "$13.210,00")
+    ("Midmarket" "France" 2178 "$3,00" "$15,00" "$32.670,00" "$10.890,00")
+    ("Midmarket" "Germany" 888 "$3,00" "$15,00" "$13.320,00" "$4.440,00")
+    ("Midmarket" "Mexico" 2470 "$3,00" "$15,00" "$37.050,00" "$12.350,00")))
+
+(define (list->table l)
+  (lambda (x y)
+    (list-ref (list-ref l y) x)))
+
+(define unit-table (list->table unit-list))
