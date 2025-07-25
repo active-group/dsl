545311f8559448ff8884b3621d563d506ceda2fe
Mike Sperber <sperber@deinprogramm.de>
2025-07-25 11:36:00 +0200

    Update.

--- racket/macros.rkt
+++ racket/macros.rkt
@@ -71,3 +71,15 @@ z
 
 (if* (> 2 1) then "zwo" else "uno")
 ;(if* (> 2 1) dann "zwo" sonst "uno")
+
+#|
+let triangles =
+      [ (a,b,c) | c <- [1..10], b <- [1..10], a <- [1..10] ]
+
+let rightTriangles =
+      [ (a,b,c) |
+        c <- [1..10],
+        b <- [1..c],
+        a <- [1..b],
+        a^2 + b^2 == c^2 ]
+|#
