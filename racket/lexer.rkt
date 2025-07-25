79004c0e43d33c9576cfd1c60742f74d6f628bb9
Mike Sperber <sperber@deinprogramm.de>
2025-07-25 15:12:58 +0200

    Added string-lexer

--- racket/lexer.rkt
+++ racket/lexer.rkt
@@ -7,4 +7,18 @@
 
 (define-lex-abbrev line-break #\newline)
 
+; parse string literal after opening quote
+(define string-lexer
+  (lexer
+   ((eof) (error 'tables-lexer "Unterminated string"))
+   ((:~ #\" #\\ #\newline) (cons (car (string->list lexeme))
+                                 (string-lexer input-port)))
+   ((:: #\\ #\\) (cons #\\ (string-lexer input-port)))
+   ((:: #\\ #\newline) (cons #\newline (string-lexer input-port)))
+   ((:: #\\ #\") (cons #\" (string-lexer input-port)))
+   (#\" '())))
+
+(define (get-string input-port)
+  (list->string (string-lexer input-port)))
+
 ; https://github.com/racket/datalog/blob/master/private/lex.rkt
