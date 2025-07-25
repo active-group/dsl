fa08ba7f3e6bf3578162ca979aa98c95ed0314e2
Mike Sperber <sperber@deinprogramm.de>
2025-07-25 16:06:55 +0200

    Skeleton parser definition

--- racket/parser.rkt
+++ racket/parser.rkt
@@ -14,3 +14,21 @@
         (position-col start-pos)
         (position-offset start-pos)
         (- (position-offset end-pos) (position-offset start-pos))))
+
+(define-values (table-definition-parser)
+  (apply
+   values
+   (parser
+    (start table-definition)
+    (end EOF)
+    (tokens tokens-with-payload plain-tokens)
+    (src-pos)
+    (error
+     (lambda (ok? name value start-pos end-pos)
+       (raise-syntax-error 'tables
+                           (if ok?
+                               (format "Unexpected token ~S" name)
+                               (format "Invalid token ~S" name))
+                           (datum->syntax #f value (make-srcloc start-pos end-pos)))))
+    (grammar
+     ...))))
