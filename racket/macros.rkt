8944904725ba7d92b2c52bb4ea4302b66d231208
Mike Sperber <sperber@deinprogramm.de>
2025-07-25 11:21:25 +0200

    Update.

--- racket/macros.rkt
+++ racket/macros.rkt
@@ -1,1 +1,33 @@
 #lang racket
+(provide swap!)
+#;(define (swap! x y)
+  (let ((z x))
+    (set! x y)
+    (set! y z)))
+
+; Makro
+(define-syntax-rule (swap! x y)
+  (let ((z x)) ; neue Variable
+    (set! x y)
+    (set! y z)))
+
+(define y 23)
+(define z 42)
+(swap! y z)
+y
+z
+
+(define-syntax-rule (my-min x y)
+  (let ((x* x)
+        (y* y))
+    (if (< x* y*)
+        x*
+        y*)))
+
+(define (fib n)
+  (cond
+    ((= n 0) 1)
+    ((= n 1) 1)
+    (else (+ (fib (- n 1)) (fib (- n 2))))))
+
+;(my-min (fib 23) (fib 50))
