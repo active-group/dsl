928fd4ebc7d336d36f843c50ca553fa533f96d09
Tobias Ammann <tobias.ammann@bedag.ch>
2025-07-25 16:27:24 +0200

    parser

--- racket/parser.rkt
+++ racket/parser.rkt
@@ -15,20 +15,31 @@
         (position-offset start-pos)
         (- (position-offset end-pos) (position-offset start-pos))))
 
-(define-values (table-definition-parser)
-  (apply
-   values
-   (parser
-    (start table-definition)
-    (end EOF)
-    (tokens tokens-with-payload plain-tokens)
-    (src-pos)
-    (error
-     (lambda (ok? name value start-pos end-pos)
-       (raise-syntax-error 'tables
-                           (if ok?
-                               (format "Unexpected token ~S" name)
-                               (format "Invalid token ~S" name))
-                           (datum->syntax #f value (make-srcloc start-pos end-pos)))))
-    (grammar
-     ...))))
+#|
+  DEFINE TABLE t3 WITH StructName (STR segment "Segment", STR country "Country")
+
+  (get-tokens-from-string "DEFINE TABLE t3 WITH StructName (STR segment \"Segment\", STR country \"Country\")")
+  |#
+
+(define table-definition-parser
+  (parser
+   (start table-definition)
+   (end EOF)
+   (tokens tokens-with-payload plain-tokens)
+   (src-pos)
+   (error
+    (lambda (ok? name value start-pos end-pos)
+      (raise-syntax-error 'tables
+                          (if ok?
+                              (format "Unexpected token ~S" name)
+                              (format "Invalid token ~S" name))
+                          (datum->syntax #f value (make-srcloc start-pos end-pos)))))
+   (grammar
+    (table-definition ((DEFINE TABLE IDENTIFIER WITH IDENTIFIER LPAREN field-definitions RPAREN)  (table-definition $3 $5 $7)))
+    (field-definitions (() '())
+                       ((field-definition COMMA field-definitions) (cons $1 $3)))
+    (field-definition ((type IDENTIFIER STRING) (field-definition $1 $2 $3)))
+    (type ((STR) 'STR)
+          ((INT) 'INT)
+          ((CURRENCY) 'CURRENCY)))))
+                                                                                                   
