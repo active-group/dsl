c65eab0f7e9e7d842ec0a7582598bcf1d89486d9
Mike Sperber <sperber@deinprogramm.de>
2026-04-13 15:43:33 +0200

    Update.

---
+++ racket/sets.rkt
@@ -0,0 +1,51 @@
+#lang racket
+; Eine Menge ist eins der folgenden:
+; X = { x \in A | x <= 5 }
+
+; - eine Einschränkung einer Menge  -ODER-
+; - eine Liste aller Elemente (Reihenfolge spielt keine Rolle)
+
+(struct list-set
+  (elements) ; Liste der Elemente
+  #:transparent)
+
+(struct restricted-set
+  (restrictions ; Liste von Einschränkungen
+   set)
+  #:transparent)
+
+; Eine Einschränkung ist ein Prädikat,
+; das sagt, ob ein Element drin ist oder nicht.
+; 1. Versuch: Funktion: Element rein, Boolean raus
+
+(define set10
+  (list-set (list 1 2 3 4 5 6 7 8 9 10)))
+
+(define evens10
+  (restricted-set (list even?)
+                  set10))
+
+(define set<5
+  (restricted-set (list (lambda (x) (< x 5)))
+                  set10))
+
+(define set???
+  (restricted-set (list (lambda (x) (< x 5)) even?)
+                  set10))
+
+; Kompositionalität:
+; Die Bedeutung eines Gegenstands hängt nur von der Bedeutung
+; seiner Bestandteile ab.
+
+; Semantik:
+; Elemente einer Menge
+(define (set-elements set)
+  (match set
+    ((list-set elements) elements)
+    ((restricted-set restrictions inner-set)
+     (filter (lambda (element)
+               ... restrictions ...)
+             (set-elements inner-set)))))
+
+(define (fulfills-all? predicates x)
+  
