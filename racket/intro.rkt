6a016a42c1e83fca207c04cfdc3bbedc2f4acaf0
Mike Sperber <sperber@deinprogramm.de>
2026-07-13 11:28:05 +0200

    Update.

--- racket/intro.rkt
+++ racket/intro.rkt
@@ -1,2 +1,14 @@
 #lang racket
+(require 2htdp/image) ; "How to do Design Programs"
+
 ; Mike sein Code
+(define x
+  (+ 23
+     (* 2
+        21)))
+
+(define circle1 (circle 50 "solid" "red"))
+
+(define square1 (square 100 "outline" "gold"))
+
+(define star1 (star 50 "solid" "green"))
