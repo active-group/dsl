c7355f8aef1a5cb759d89211a4c4160120afda76
Mike Sperber <sperber@deinprogramm.de>
2026-07-13 11:36:49 +0200

    Update.

--- racket/intro.rkt
+++ racket/intro.rkt
@@ -15,3 +15,23 @@
 
 (define y 'mike) ; Symbol: "enum"
 
+(define o1 (overlay star1 circle1))
+
+(above
+ (beside star1 circle1)
+ (beside circle1 star1))
+
+(above
+ (beside circle1 square1)
+ (beside square1 circle1))
+
+; Abstraktion: 2 Code-Fragmente, sehr ähnlich
+; 1. (ein letztes Mal) kopieren
+; 2. Unterschiede ersetzen durch (abstrakte) Namen
+; 3. neue Namen in lambda aufnehmen
+
+(define tile
+  (lambda (image1 image2)
+    (above
+     (beside image1 image2)
+     (beside image2 image1))))
