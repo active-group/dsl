bd6c6401a1427beb37107d37ffddab4e61c75518
Mike Sperber <sperber@deinprogramm.de>
2026-07-13 11:29:46 +0200

    Update.

--- racket/intro.rkt
+++ racket/intro.rkt
@@ -7,8 +7,11 @@
      (* 2
         21)))
 
-(define circle1 (circle 50 "solid" "red"))
+(define circle1 (circle 50 'solid "red"))
 
-(define square1 (square 100 "outline" "gold"))
+(define square1 (square 100 'outline "gold"))
 
-(define star1 (star 50 "solid" "green"))
+(define star1 (star 50 'solid "green"))
+
+(define y 'mike) ; Symbol: "enum"
+
