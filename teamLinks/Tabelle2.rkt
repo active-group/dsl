97db30022f437315036566ab8af44fdf9da1139f
NikolaVeber <nikola.veber@gmail.com>
2026-07-14 13:54:57 +0200

    Enhance table-read function and formats
    Added support for additional table formats and improved error handling in table-read function.

--- teamLinks/Tabelle2.rkt
+++ teamLinks/Tabelle2.rkt
@@ -131,6 +131,180 @@ format1
 
 (define (table-read format table row column)
   (match format
+    ((header-cell-format name)
+     (define cell-content (table-ref table row column))
+     (if (equal? name cell-content)
+         cell-content
+         (error 'table-ref "header name does not equal the one defined in format: ~a  /=  ~a" name cell-content)
+         ))
+    ((cell-format type)
+     (define cell-content (table-ref table row column))
+     (match type 
+       ('string
+        (if (string? cell-content)
+            cell-content
+            (error 'table-ref "cell content is not a string: ~a" cell-content)))
+       ('number
+        (if (number? cell-content)
+            cell-content
+            (error 'table-ref "cell content is not a number: ~a" cell-content)))
+                     
+       ))
+    ((record-format formats direction constructor)
+     (define (loop formats row column)
+       (match formats
+         ('() '())
+         ((cons format rest-formats)
+          (cons (table-read format table row column)
+                (loop rest-formats table row column))))) ; TODO row und column stimmen nicht
+     (apply constructor (loop formats )))
+    ((list-format elemtn-format direction) 'todo)))
+
+
+
+
+(define table1
+  '(("Segment" "Country" "Units Sold" "Manuf. Price" "Sale Price")
+    ("Government" "Canada" 1618 3 20)
+    ))
+
+table1
+(table-read (cell-format 'string) '(("Mike")) 0 0)
+(table-read (header-cell-format "Country") '(("Country")) 0 0)
+
+ #lang racket
+
+
+; tabellenformat ist eins von:
+; header cell ODER
+; cell ODER
+; record ODER
+; liste 
+
+(struct header-cell-format
+  (name)
+  #:transparent)
+
+(struct cell-format
+  (type
+   )
+  #:transparent)
+
+(struct record-format
+  (formats ; liste von irgendwelchen values / formaten / namen, whatever
+   direction ; 'down oder 'right
+   constructor) ; ... vom struct
+  #:transparent)
+
+(struct list-format
+  (element-format
+   direction)
+  #:transparent
+  )
+
+
+
+; --- ^^^^^ DSL
+
+
+#;(struct entryList ; beschreibt ein row mit beliebig vielen komponenten
+  (entries ; list von entries
+   );#transparent
+   )
+
+(struct prices
+  (manufacturing sale)
+  #:transparent)
+
+(define price-format
+  (record-format (list (cell-format 'number)
+                (cell-format 'number))
+          'down
+          prices))
+
+
+
+
+(define header
+  (record-format (list (header-cell-format "Segment")
+                (header-cell-format "Country")
+                (header-cell-format "Units Sold")
+                (header-cell-format "Prices"))
+          'right
+          list))
+
+
+
+(struct entry  ; beschreibt ein row, mit fixen anzahl von komponenten
+  (segment country units-sold prices)
+  #:transparent)
+
+
+(define format1
+  (record-format (list (cell-format 'string)
+                       (cell-format 'string)
+                       (cell-format 'number)
+                       price-format)
+          'right
+          entry))
+
+
+
+format1
+
+
+(define table-format
+  (record-format (list header
+                (list-format format1 'down))
+          'down
+          (lambda (header-content payload)
+            payload)))
+
+
+
+
+(define (table-ref table row column)
+  (list-ref (list-ref table row) column))
+
+
+
+
+
+(define header2
+  (record-format (list (header-cell-format "Segment")
+                (header-cell-format "Country")
+                (header-cell-format "Units Sold")
+                (header-cell-format "Manuf. price")
+                (header-cell-format "Sale price"))
+          'right
+          list))
+
+(struct entry2  ; beschreibt ein row, mit fixen anzahl von komponenten
+  (segment country units-sold manufacturing-price sales-proce)
+  #:transparent)
+
+
+(define format2
+  (record-format (list (cell-format 'string)
+                       (cell-format 'string)
+                       (cell-format 'number)
+                       (cell-format 'number)
+                       (cell-format 'number))
+          'right
+          entry2))
+
+
+
+(define table-format2
+  (record-format (list header2
+                       format2)
+          'down
+          (lambda (header-content payload)
+            payload)))
+
+
+(define (table-read format table row column)
+  (match format
     ((header-cell-format name) 'todo )
     ((cell-format type)
      (define cell-content (table-ref table row column))
