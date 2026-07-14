a7b9071e3b2a7e44831e28932b6fe221bd4bc734
Mike Sperber <sperber@deinprogramm.de>
2026-07-14 12:12:54 +0200

    More data in example table

--- racket/table-code.rkt
+++ racket/table-code.rkt
@@ -3,7 +3,10 @@
 (define table1
   '(("Segment" "Country" "Units Sold" "Manuf. Price" "Sale Price")
     ("Government" "Canada" 1618 3 20)
-    ))
+    ("Government" "Germany" 1321 3 20)
+    ("Midmarket" "France" 2178 3 15)
+    ("Midmarket" "Germany" 888 3 15)
+    ("Midmarket" "Mexico" 2470 	3 15)))
 
 (struct entry
   (segment
